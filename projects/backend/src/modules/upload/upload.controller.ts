f6e84484f898b2dbaa9c18752e1348cad830f7ad
Chanichkorn sornsongkram <nemosornsongkram@gmail.com>
2025-09-30 11:55:41 +0700

    add dto & swagger

--- projects/backend/src/modules/upload/upload.controller.ts
+++ projects/backend/src/modules/upload/upload.controller.ts
@@ -1,14 +1,37 @@
 import { Controller, Post, UploadedFile, UseInterceptors  } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiConsumes, ApiBody, ApiParam } from '@nestjs/swagger';
+import { FileUploadResponseDto } from './dto/upload.dto';
 
+@ApiTags('Upload')
 @Controller('upload')
 export class UploadController {
     constructor(private readonly uploadService: UploadService) {}
 
     @Post('/upload')
     @UseInterceptors(FileInterceptor('file'))
-    async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    @ApiOperation({ summary: 'Upload a single file' })
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+    description: 'The file to upload',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({ 
+    status: 201, 
+    description: 'File has been successfully uploaded.',
+    type: FileUploadResponseDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request. No file provided.' })
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
         return this.uploadService.handleFileUpload(file);
      
     }
