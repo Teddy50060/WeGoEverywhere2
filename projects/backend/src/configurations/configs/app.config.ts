889176c3f26a6cb26ee02f1685fd27a77771cd7e
Thara Waranuset <tharawaranuset@gmail.com>
2025-10-02 16:56:48 +0700

    refactor config

--- projects/backend/src/configurations/configs/app.config.ts
+++ projects/backend/src/configurations/configs/app.config.ts
@@ -1,7 +1,7 @@
-import { AppConfig } from '../interfaces/app-config';
 
-export default (): AppConfig => ({
+export default () => ({
   app: {
+    frontendUrl: process.env.FRONTEND_APP_URL || 'http://localhost:3000',
     port: parseInt(process.env.BACKEND_APP_PORT ?? '3001', 10),
   },
 });
--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get<string>('app.frontendUrl') || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // cookie
   });
