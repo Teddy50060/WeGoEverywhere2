c4ee50ec21ad2792cd1b8848b282e8b4102a21ed
kaiartty <pangpangpit.art@gmail.com>
2025-11-03 00:05:36 +0700

    bugfix/user can edit other event

--- projects/frontend/src/app/event/page.tsx
+++ projects/frontend/src/app/event/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Navbar } from "@/components/navbar/Navbar";
 import { getAllEvents } from "@/actions/actions";
+import EventDeniedToastOnce from "@/components/system/EventDeniedToastOnce";
 
 type EventApi = {
   eventId: number;
@@ -19,6 +20,8 @@ export default async function EventPage() {
 
   return (
     <>
+      <EventDeniedToastOnce />
+
       <div className="flex justify-center mt-4 font-black text-5xl">
         Event Page
       </div>
@@ -51,24 +54,6 @@ export default async function EventPage() {
       <div className="flex justify-center mt-4 font-semibold text-2xl pb-20">
         Link to EventPage
       </div>
-
-      {/* <div className="flex flex-col items-center mt-4 text-lg font-medium">
-        {events.length === 0 ? (
-          <div className="text-gray-500">ยังไม่มีอีเวนต์</div>
-        ) : (
-          events.map((ev) => (
-            <Link
-              href={`/event/${ev.eventId}`}
-              key={ev.eventId}
-              className="mb-2"
-            >
-              <div key={ev.eventId}>
-                {ev.name} ({ev.eventId})
-              </div>
-            </Link>
-          ))
-        )}
-      </div> */}
 
       <Navbar />
     </>
