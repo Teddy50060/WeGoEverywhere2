3b949837c87100885dd0083cea6e5bfe32ca75f0
Teddy's playground <164116423+Teddy50060@users.noreply.github.com>
2025-11-16 22:58:19 +0700

    first commit

--- projects/backend/src/main.ts
+++ projects/backend/src/main.ts
@@ -17,13 +17,19 @@ async function bootstrap() {
   setupUploads(app); //upload pic
 
   app.enableCors({
-    origin: configService.get<string>('app.frontendUrl') || 'http://localhost:3000',
+    origin: [
+      configService.get<string>('app.frontendUrl') || 'http://localhost:3000',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // cookie
   });
   setupSwagger(app, configService);
 
-  const port = configService.get<number>('app.port', 3001);
-  await app.listen(port);
+  const port = process.env.PORT || configService.get<number>('app.port', 3001);
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
