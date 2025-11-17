6a4989de0dcdd56749c49a3b4b4f030fcb44bb64
Teddy's playground <164116423+Teddy50060@users.noreply.github.com>
2025-11-17 16:29:32 +0700

    debugging socket_url

--- projects/frontend/src/configurations/config/socket.ts
+++ projects/frontend/src/configurations/config/socket.ts
@@ -1,2 +1,6 @@
 export const SOCKET_URL =
   process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001";
+
+console.log(' Socket URL:', SOCKET_URL);
+console.log(' Env var:', process.env.NEXT_PUBLIC_SOCKET_URL);
+console.log(' Next URL',process.env.NEXT_PUBLIC_API_URL);
